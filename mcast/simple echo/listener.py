4d847b0a40cc50fa9ec19b564e7cf8430a908690
Sylvain Firmery <sfirmery@gmail.com>
2013-02-12 14:48:08 +0100

    Simple echo with multicast

---
+++ mcast/simple echo/listener.py
@@ -0,0 +1,31 @@
+import socket
+
+MCAST_GRP = "239.18.0.10"
+UDP_PORT = 9000
+
+# create udp socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# define multicast params
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+try:
+  sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+except AttributeError:
+  pass # Some systems don't support SO_REUSEPORT
+sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_TTL, 1)
+sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_LOOP, 1)
+
+# bind 
+sock.bind(('', UDP_PORT))
+
+# Set some more multicast options
+sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton("0.0.0.0"))
+sock.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(MCAST_GRP) + socket.inet_aton("0.0.0.0"))
+
+while 1:
+  data, addr = sock.recvfrom(1024)
+  print "received message:", data, "from:", addr
+  
+  sock.sendto("Re:" + data, addr)
+
---
+++ mcast/simple echo/sender.py
@@ -0,0 +1,36 @@
+import socket
+import time
+
+MCAST_GRP = "239.18.0.10"
+UDP_PORT = 9000
+MESSAGE = "Multicast sandbox"
+
+# create udp socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# define multicast params
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# set timeout
+sock.settimeout(.2)
+
+# send data many times
+while 1:
+  # send data to multicast group
+  sock.sendto(MESSAGE, (MCAST_GRP, UDP_PORT))
+
+  print "send message:", MESSAGE, "to:", MCAST_GRP
+  print "waiting reply..."
+
+  # listen reply
+  while 1:
+    try:
+      data, addr = sock.recvfrom(12)
+    except socket.timeout:
+      print 'timed out, no more responses'
+      break
+    else:
+      print "received message:", data, "from:", addr
+
+  time.sleep(1)
